2705896d540aaeff4ce9ad69493c4a8ec65d7a4b
Destry781 <80601365+Destry781@users.noreply.github.com>
2021-03-22 14:01:15 -0400

    Update AssemblyInfo.cs

--- StormFN Launcher/Properties/AssemblyInfo.cs
+++ StormFN Launcher/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StormFN Launcher")]
+[assembly: AssemblyProduct("GlossyFN Launcher")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
