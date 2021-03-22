3d5d84aaabe075bc2aa8964b7d0a7c905849ef05
Destry781 <80601365+Destry781@users.noreply.github.com>
2021-03-22 14:03:35 -0400

    Update Settings.Designer.cs

--- StormFN Launcher/Properties/Settings.Designer.cs
+++ StormFN Launcher/Properties/Settings.Designer.cs
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 
-namespace StormFN_Launcher.Properties
+namespace GlossyFN_Launcher.Properties
 {
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
